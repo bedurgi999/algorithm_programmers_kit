1169caeffd0ff038722f6b1f5551a088e4cb7260
BoSeon Park <bedurgi999@gmail.com>
2022-07-26 16:05:37 +0900

    프로그래머스 고득점 kit 완전탐색 소수찾기 문제 level 2

---
+++ brute-force/소수찾기.py
@@ -0,0 +1,31 @@
+# 에라토스테네스 체를 사용한 문제풀이
+# 결과 : 시간초과
+from itertools import permutations
+
+def Eratos(num):
+    sieve = [True] * num
+    
+    n = int(num ** 0.5)
+    
+    for i in range(2, n+1):
+        if sieve[i] == True:
+            for j in range(i+i, num, i):
+                sieve[j] = False
+    
+    return [i for i in range(2, num) if sieve[i] == True]
+
+def solution(numbers):
+    answer = 0
+    temp = []
+    
+    nums = list(numbers)
+    e = Eratos(int(''.join(sorted(nums, reverse=True)))+1)
+    for i in range(1, len(nums) + 1):
+        num_list = permutations(nums, i)
+        for num in num_list:
+            a = int(''.join(list(num)))
+            if a in e:
+                temp.append(a)
+                
+    answer = len(list(set(temp)))
+    return answer
---
+++ brute-force/소수찾기_2.py
@@ -0,0 +1,28 @@
+# 제곱근까지의 숫자로 나누어보기
+# 결과 : 정답
+from itertools import permutations
+
+def solution(numbers):
+    answer = 0
+
+    answer_list = []
+    num = list(numbers)
+    for i in range(1, len(num)+1):
+        num_list = permutations(num, i)
+        for n in num_list:
+            check = True
+            temp = int(''.join(list(n)))
+            if temp == 0 or temp == 1:
+                continue
+            
+            s = int(temp ** 0.5 + 1)
+            
+            for j in range(2, s):
+                if temp % j == 0:
+                    check = False
+                    break
+            if check:
+                answer_list.append(temp)
+                
+    answer = len(list(set(answer_list)))
+    return answer
